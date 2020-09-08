a4d4085bd88c15b878d3dafbdd2b788d7877e266
Olivier Picard <olivier_picard@icloud.com>
2020-09-09 01:02:25 +0400

    Creation of a script to start oanda program

---
+++ institutions/oanda/scripts/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+dirpath=$(dirname $(which $0))
+src=$dirpath/..
+
+export GOOGLE_APPLICATION_CREDENTIALS="/etc/brushed-charts/credentials/backend-institution_account-service.json"
+export OANDA_API_URL="https://api-fxpractice.oanda.com"
+export OANDA_BIGQUERY_SHORTTERM_TABLENAME="price_shortterm"
+export OANDA_BIGQUERY_ARCHIVE_TABLENAME="price_archive"
+export OANDA_BIGQUERY_DATASET="oanda_dev"
+export OANDA_WATCHLIST_PATH="./resources/oanda_watchlist.txt"
+
+#go run $src > "$src"/../../logs/oanda.log 2>&1 
+#oanda_PID=$!
+#echo $oanda_PID > ./var/PID
+
+go run $src
