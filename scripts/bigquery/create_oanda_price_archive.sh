d96a9973b4c3e3ec167de34b321f28d0ace77bb3
Olivier Picard <olivier_picard@icloud.com>
2020-09-08 18:01:21 +0400

    Improve bash script to create BQ dev and prod tables

--- scripts/bigquery/create_oanda_price_archive.sh
+++ scripts/bigquery/create_oanda_price_archive.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Table expiration 10 years. 
-bq mk -t \
---expiration 0 \
---time_partitioning_field date \
---time_partitioning_type=DAY \
---time_partitioning_expiration 315400000 \
---require_partition_filter \
---clustering_fields interval,instrument \
-oanda.price_archive \
-./candle_bidask_shemas.json
+function create {
+    bq mk -t \
+    --expiration 0 \
+    --time_partitioning_field date \
+    --time_partitioning_type=DAY \
+    --time_partitioning_expiration $2 \
+    --require_partition_filter \
+    --clustering_fields interval,instrument \
+    $1 \
+    ./candle_bidask_shemas.json
+}
+
+create "oanda_prod.price_archive" 315400000 # 10 years
+create "oanda_dev.price_archive" 1210000 # 2 weeks
--- scripts/bigquery/create_oanda_price_shortterm.sh
+++ scripts/bigquery/create_oanda_price_shortterm.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-# Table expiration 10 years. 
-bq mk -t \
---expiration 0 \
---time_partitioning_field date \
---time_partitioning_type=HOUR \
---time_partitioning_expiration 13140000 \
---require_partition_filter \
---clustering_fields interval,instrument \
-oanda.price_shortterm \
-./candle_bidask_shemas.json
+function create {
+    # Table expiration 5 month. 
+    bq mk -t \
+        --expiration 0 \
+        --time_partitioning_field date \
+        --time_partitioning_type=HOUR \
+        --time_partitioning_expiration "$2" \
+        --require_partition_filter \
+        --clustering_fields interval,instrument \
+        "$1" \
+        ./candle_bidask_shemas.json
+}
+
+create "oanda_prod.price_shortterm" 13140000 # 5 months
+create "oanda_dev.price_shortterm" 1210000 # 2 weeks
+
