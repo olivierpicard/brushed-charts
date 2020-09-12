050210121fb7fb79ff5e587869ff110d8ed45d54
Olivier Picard <olivier_picard@icloud.com>
2020-09-13 03:13:04 +0400

    Fix a profil bug

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,7 +7,7 @@ export BCBPATH=$PWD # Brushed-Charts base path
 export GOOGLE_APPLICATION_CREDENTIALS="/etc/brushed-charts/credentials/backend-institution_account-service.json"
 
 echo -n "Choose a profile (dev|test|prod) [dev]: "
-read -r profile
+read -r profil
 if [ -z $profil ]
 then
     profil="dev"
