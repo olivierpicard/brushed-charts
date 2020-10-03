f5f4387b3040a63f2ea553593bf6c7bff79bace5
Olivier Picard <olivier_picard@icloud.com>
2020-10-03 11:09:12 +0400

    Add integration and unit tests to the main package

--- institutions/oanda/src/main.go
+++ institutions/oanda/src/main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/brushed-charts/backend/institutions/oanda/src/account"
+	"github.com/brushed-charts/backend/lib/cloudlog"
+	"github.com/pkg/errors"
+)
+
+func main() {
+	initGlobalVariables()
+	_, err := getOandaAccountID()
+	if err != nil {
+		cloudlog.Panic(err)
+	}
+}
+
+func getOandaAccountID() (string, error) {
+	oandaAccountID, err := account.GetAccountID(&http.Client{})
+	if err != nil {
+		return "", err
+	}
+
+	if oandaAccountID == "" {
+		err = errors.New("Oanda account ID is empty")
+		return "", err
+	}
+	return oandaAccountID, nil
 }
--- institutions/oanda/src/main_test.go
+++ institutions/oanda/src/main_test.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"os"
 	"testing"
+
+	"github.com/tj/assert"
 )
 
-func TestSayHello(t *testing.T) {
+func init() {
+	os.Setenv(envKeyEnvironmentMode, "dev")
+}
 
+func TestIntegration_AccountID_NotDevEnv(t *testing.T) {
+	previousEnvMode := os.Getenv(envKeyEnvironmentMode)
+	os.Setenv(envKeyEnvironmentMode, "integration_test")
+	defer os.Setenv(envKeyEnvironmentMode, previousEnvMode)
+
+	testAccountID(t)
+}
+func TestIntegration_AccountID_DevEnv(t *testing.T) {
+	previousEnvMode := os.Getenv(envKeyEnvironmentMode)
+	os.Setenv(envKeyEnvironmentMode, "dev")
+	defer os.Setenv(envKeyEnvironmentMode, previousEnvMode)
+
+	testAccountID(t)
+}
+
+func testAccountID(t *testing.T) {
+	accountID, err := getOandaAccountID()
+	assert.Nil(t, err)
+	assert.NotEmpty(t, accountID)
 }
