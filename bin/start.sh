adf762b0c737701080eeaa3c680da598642bf3b5
Olivier Picard <olivier_picard@icloud.com>
2020-12-06 01:28:02 +0400

    test

--- bin/start.sh
+++ bin/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+export GOOGLE_APPLICATION_CREDENTIALS="/etc/brushed-charts/backend-institution_account-service.json"
+
 dirpath=$(dirname $(which $0))
 cd "$dirpath"/..
 
 echo -n "Choose a profile (dev|test|prod) [dev]: "
 read -r profil
-
 if [ -z $profil ]; then
     profil="dev"
 fi
