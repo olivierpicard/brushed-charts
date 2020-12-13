544d093b6256ad6362f33f6082c7b6a2548cacb3
Olivier Picard <olivier_picard@icloud.com>
2020-12-13 12:19:40 +0400

    Check if secret env variables are not empty

--- bin/make_oanda_env_file.sh
+++ bin/make_oanda_env_file.sh
@@ -9,6 +9,9 @@ ENV_FILE_PATH="env/oanda_secret.env"
 API_TOKEN=$(bin/get_oanda_api_token.sh) || exit 1 
 ACCOUNT_ID=$(bin/get_oanda_account_id.sh) || exit 1
 
+[ -z API_TOKEN ] || echo "Can't retrieve API_TOKEN"; exit 3
+[ -z ACCOUNT_ID ] || echo "Can't retrieve ACCOUNT_ID"; exit 3
+
 rm -f $ENV_FILE_PATH || exit 2
 touch $ENV_FILE_PATH || exit 2
 
