aa99dfed73763ee3faabbd45981819c6b6a22578
Olivier Picard <olivier_picard@icloud.com>
2021-01-23 19:08:35 +0400

    Graphql local deployment has access to test dataset

--- services/oanda_graphql/start_local.sh
+++ services/oanda_graphql/start_local.sh
@@ -10,7 +10,7 @@ docker run \
     --restart always \
     --name oanda_graphql_local \
     --env-file env/services.env \
-    --env-file env/services.dev.env \
+    --env-file env/services.test.env \
     -v /etc/brushed-charts/backend-institution_account-service.json:/etc/brushed-charts/backend-institution_account-service.json \
     -p 3330:3330 \
     -d oanda_graphql
