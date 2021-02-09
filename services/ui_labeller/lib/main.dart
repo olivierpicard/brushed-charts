947ede74b696b2c5b21e386861209f706a016985
Olivier Picard <olivier_picard@icloud.com>
2021-02-09 22:22:01 +0400

    Add some algorithms for the grapher...

--- services/ui_labeller/lib/main.dart
+++ services/ui_labeller/lib/main.dart
@@ -1,22 +1,21 @@
 import 'backend_link/main.dart' as graphql;
 import 'package:flutter/material.dart';
-import 'package:labeling_helper/scene.dart';
+import 'scene.dart';
 
 main(List<String> args) async {
   runApp(App());
-  final variables = graphql.VariablesGetCandles();
-  variables.dateFrom = "2021-01-20 10:00:00";
-  variables.dateTo = "2021-01-20 10:10:00";
-  variables.instrument = "EUR_USD";
-  variables.granularity = "S5";
-  List<graphql.DataCandle> candles;
+  // final variables = graphql.VariablesGetCandles();
+  // variables.dateFrom = "2021-01-20 10:00:00";
+  // variables.dateTo = "2021-01-20 10:10:00";
+  // variables.instrument = "EUR_USD";
+  // variables.granularity = "S5";
+  // List<graphql.DataCandle> candles;
 
-  try {
-    candles = await graphql.getCandles(variables);
-    print(candles.mid.open);
-  } on graphql.ResponseError catch (e) {
-    print(e.cause);
-  }
+  // try {
+  //   candles = await graphql.getCandles(variables);
+  // } on graphql.ResponseError catch (e) {
+  //   print(e.cause);
+  // }
 }
 
 class App extends StatelessWidget {
--- services/ui_labeller/lib/scene.dart
+++ services/ui_labeller/lib/scene.dart
@@ -1,4 +1,4 @@
-import './backend_link/main.dart' as graphql;
+import 'backend_link/main.dart' as graphql;
 import 'package:flutter/material.dart';
 
 class Scene implements CustomPainter {
@@ -31,3 +31,84 @@ class Scene implements CustomPainter {
   @override
   bool shouldRebuildSemantics(covariant CustomPainter oldDelegate) => false;
 }
+
+
+ /*
+    scene = Scene(<options>)
+    candles = list()
+    formattedCandles = [{time: <timestamp>, open: <double>, high: <double>, low: <double>, close: <double> }]
+    candleGraph = scene.addCandles(formattedCandles)
+    line = scene.addLine(xStart: <timestamp>, yStart: <double>, xEnd: <timestamp>, yEnd: <timestamp>)
+    
+    scene = Scene()
+    candles = list()
+    List formattedCandles = [{time: <timestamp>, open: <double>, high: <double>, low: <double>, close: <double> }]
+    candleLayer = CandleLayer(formattedCandles)
+    scene.add(candleLayer)
+    lineLayer = LineLayer(xStart: <timestamp>, yStart: <double>, xEnd: <timestamp>, yEnd: <timestamp>)
+    scene.add(lineLayer)
+
+    Layer {
+      draw(c)
+      hitTest()
+      zindex: <double>
+    }
+
+    CandleLayer: Layer {
+      List candlesData
+      List<CandleDrawable> candlesView
+
+      CandleLayer(this.candle)
+      
+      draw(cursor) {
+        candleView.reset()
+        foreach candleData {
+          cursor.next()
+          candle = Geometry.buildCandle(cursor: <GraphCursor>, OHLC: <OHLC> ): CandleDrawable
+          candleView.add(candle)
+        }
+      }
+      
+      hitTest(Offset) {
+        foreach candleView {
+          if candle.hittest(Offset) {
+            return true
+          }
+        }
+      }
+    }
+
+    Geometry {
+      percentMarginLR = 3
+      buildCandle(cursor: <Cursor>, OHLC: <OHLC>) {
+      }
+
+      makeBody() {
+        LR = makeLeftRight(cursor)
+        open = cursor.scene.yaxis.dataToPixel(OHLC.open)
+        close = cursor.scene.yaxis.dataToPixel(OHLC.close)
+        cursor.canvas.drawRect = 
+      }
+
+      makeBodyColor() {
+        cursor.scene.theme.candle.up
+      }
+
+      makeLeftRight(cursor: <Cursor>) {
+        cumuledMarginLR = 3% of cursor.column.width
+        margin = cumuledMarginLR / 2
+        candleWidth = cursor.column.width - marginLR
+        left = cursor.column.left + margin
+        right = cursor.column.right - margin
+
+        return Offset(left, right)
+      }
+
+      Cursor {
+        scene
+        unit
+        
+      }
+    }
+    -
+    */
