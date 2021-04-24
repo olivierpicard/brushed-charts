27d106067240553c1ad6747ed35243105d022c68
Olivier Picard <olivier_picard@icloud.com>
2021-04-24 16:00:22 +0400

    Add kraken price struct maker/remover for bigquery

--- bin/bigquery/create_kraken_prices.sh
+++ bin/bigquery/create_kraken_prices.sh
@@ -10,19 +10,16 @@ function create {
         --time_partitioning_type=HOUR \
         --time_partitioning_expiration "$2" \
         --require_partition_filter \
-        --clustering_fields granularity,instrument,provider \
+        --clustering_fields granularity,instrument \
         "$1" \
-        ./prices_schema.json
+        ./kraken_prices_schema.json
 }
 
 bq ls prod || bq --location=EU mk prod
-create "prod.oanda_prices" 315400000 # 10 years
-create "prod.kraken_prices" 315400000 # 10 years
+create "prod.kraken_prices" 315400000  # 10 years
 
 bq ls "test" || bq --location=EU mk "test"
-create "test.oanda_prices" 5256000 # 2 months
 create "test.kraken_prices" 5256000 # 2 months
 
 bq ls dev || bq --location=EU mk dev
-create "dev.oanda_prices" 1210000 # 2 weeks
-create "dev.kraken_prices" 1210000 # 2 weeks
+create "dev.kraken_prices" 1210000  # 2 weeks
