d4b137c39dd7d3b4640beb75b0bcd8966130f48e
Olivier Picard <olivier_picard@icloud.com>
2021-05-01 21:13:58 +0400

    Clean seconds from mongo oanda after 10 days

--- services/oanda_clean_history/history.py
+++ services/oanda_clean_history/history.py
@@ -1,39 +1,52 @@
 import pymongo
 import os
-from typing import List, Dict
 from datetime import datetime
-from bson import objectid
 
-HOST=os.getenv("MONGODB_HOST")
-PORT=os.getenv("MONGODB_PORT")
+HOST = os.getenv("MONGODB_HOST")
+PORT = os.getenv("MONGODB_PORT")
 DATABASE = os.getenv("MONGODB_OANDA_DBNAME")
 COLLECTION_NAME = os.getenv("MONGODB_OANDA_HISTORY_COLLECTION")
 
 
-def delete_old(from_datetime: datetime):
+def delete_old_minutes(from_datetime: datetime):
     client = connect()
     collection = get_collection(client)
-    request_filter = make_filter(from_datetime)
+    request_filter = make_minutes_filter(from_datetime)
+    collection.delete_many(request_filter)
+    disconnect(client)
+
+
+def delete_old_seconds(from_datetime: datetime):
+    client = connect()
+    collection = get_collection(client)
+    request_filter = make_seconds_filter(from_datetime)
     collection.delete_many(request_filter)
     disconnect(client)
 
 
 def connect() -> pymongo.MongoClient:
     client = pymongo.MongoClient(host=HOST, port=int(PORT))
-    
+
     return client
 
 
 def get_collection(client: pymongo.MongoClient):
     database = client.get_database(DATABASE)
     collection = database.get_collection(COLLECTION_NAME)
-    
+
     return collection
 
 
-def make_filter(date: datetime) -> Dict:
+def make_minutes_filter(date: datetime) -> dict:
     parsed_date = str(date).replace(" ", "T")
     request_filter = {"date": {"$lt": parsed_date}}
+
+    return request_filter
+
+
+def make_seconds_filter(date: datetime) -> dict:
+    parsed_date = str(date).replace(" ", "T")
+    request_filter = {"date": {"$lt": parsed_date}, "granularity": "S5"}
 
     return request_filter
 
--- services/oanda_clean_history/main.py
+++ services/oanda_clean_history/main.py
@@ -7,7 +7,9 @@ from datetime import datetime, timedelta
 
 REFRESH_RATE = 60 * 30  # In seconds
 DELETE_FROM_DATE = int(os.getenv("OANDA_CLEAN_HISTORY_FROM_DAYS"))
+DELETE_SECONDS_FROM_DATE = int(os.getenv("OANDA_CLEAN_SECONDS_FROM_HISTORY_SINCE_DAYS"))
 ENVIRONMENT = os.getenv("BRUSHED_CHARTS_ENVIRONMENT")
+
 
 def deletion_limit(days: int) -> datetime:
     datetime_limit = datetime.utcnow() - timedelta(days=days)
@@ -15,10 +17,16 @@ def deletion_limit(days: int) -> datetime:
     return datetime_limit
 
 
+def delete():
+    datetime_minutes_limit = deletion_limit(DELETE_FROM_DATE)
+    datetime_seconds_limit = deletion_limit(DELETE_SECONDS_FROM_DATE)
+    history.delete_old_minutes(datetime_minutes_limit)
+    history.delete_old_seconds(datetime_seconds_limit)
+
+
 def try_execute():
     try:
-        datetime_limit = deletion_limit(DELETE_FROM_DATE)
-        history.delete_old(datetime_limit)
+        delete()
     except Exception:
         traceback.print_exc()
         error_reporting.Client(service="oanda_history."+ENVIRONMENT).report_exception()
