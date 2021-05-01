bdcd9cc5c1f0ecc499226616ebbc2285fbce6536
Olivier Picard <olivier_picard@icloud.com>
2021-05-01 20:33:43 +0400

    Improve code style for oanda_bigquery

--- services/oanda_bigquery/main.py
+++ services/oanda_bigquery/main.py
@@ -5,7 +5,6 @@ import history
 import traceback
 
 from datetime import datetime, timedelta
-from typing import List, Dict
 from google.cloud import bigquery, error_reporting
 
 
@@ -13,49 +12,51 @@ REFRESH_RATE = 30  # In seconds
 PATH_PRICE_TABLE = os.getenv("OANDA_BIGQUERY_PATH_PRICE_TABLE")
 ENVIRONMENT = os.getenv("BRUSHED_CHARTS_ENVIRONMENT")
 
-class EmptyCandles(Exception): pass
+
+class EmptyCandles(Exception):
+    pass
 
 
-def raise_if_candles_empty(candles: List):
-  if candles == None or len(candles) == 0:
-    raise EmptyCandles
-    
+def raise_if_candles_empty(candles: list):
+    if candles is None or len(candles) == 0:
+        raise EmptyCandles
 
-def send_to_bigquery(candles: List):
-  client = bigquery.Client()
-  client.insert_rows_json(PATH_PRICE_TABLE, candles)
-  manage_bigquery_errors(response)
-  client.close()
+
+def send_to_bigquery(candles: list):
+    client = bigquery.Client()
+    client.insert_rows_json(PATH_PRICE_TABLE, candles)
+    client.close()
 
 
 def make_time_window():
-  lower_window = lastupdate_log.read()
-  current_time = datetime.utcnow()
-  upper_window = current_time - timedelta(minutes=3)
+    lower_window = lastupdate_log.read()
+    current_time = datetime.utcnow()
+    upper_window = current_time - timedelta(minutes=3)
 
-  return (lower_window, upper_window)
+    return (lower_window, upper_window)
 
 
 def execute():
-  time_window = make_time_window()    
-  upper_window = time_window[1]
-  candles = history.get_candles_from_window(time_window)
-  raise_if_candles_empty(candles)
-  send_to_bigquery(candles)
-  # lastupdate_log.save_last_reading_date(upper_window)
+    time_window = make_time_window()
+    upper_window = time_window[1]
+    candles = history.get_candles_from_window(time_window)
+    raise_if_candles_empty(candles)
+    send_to_bigquery(candles)
+    lastupdate_log.save_last_reading_date(upper_window)
 
 
 def try_to_execute():
-  try:
-    execute()
-  except EmptyCandles:
-    pass
-  except Exception:
-    traceback.print_exc()
-    error_reporting.Client(service="oanda_bigquery."+ENVIRONMENT).report_exception()
+    try:
+        execute()
+    except EmptyCandles:
+        pass
+    except Exception:
+        traceback.print_exc()
+        error_reporting.Client(service="oanda_bigquery." +
+                               ENVIRONMENT).report_exception()
 
 
 if __name__ == "__main__":
-  while True:
-    try_to_execute()
-    time.sleep(REFRESH_RATE)        
+    while True:
+        try_to_execute()
+        time.sleep(REFRESH_RATE)
