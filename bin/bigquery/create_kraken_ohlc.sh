8a3ff7370616b7d331d8e01b7d34423d4d83a718
Olivier Picard <olivier_picard@icloud.com>
2021-05-01 20:25:59 +0400

    Set kraken table name, update schema, fix partition type

--- bin/bigquery/create_kraken_ohlc.sh
+++ bin/bigquery/create_kraken_ohlc.sh
@@ -6,20 +6,20 @@ cd $dir
 function create { 
     bq mk -t \
         --expiration 0 \
-        --time_partitioning_field date \
+        --time_partitioning_field datetime \
         --time_partitioning_type=DAY \
         --time_partitioning_expiration "$2" \
         --require_partition_filter \
-        --clustering_fields instrument,granularity \
+        --clustering_fields asset_pair,granularity \
         "$1" \
-        ./oanda_prices_schema.json
+        ./kraken_ohlc_schema.json
 }
 
 bq ls prod > /dev/null || bq --location=EU mk prod
-create "prod.oanda_prices" 315400000  # 10 years
+create "prod.kraken_ohlc" 315400000  # 10 years
 
 bq ls "test" > /dev/null || bq --location=EU mk "test"
-create "test.oanda_prices" 5256000 # 2 months
+create "test.kraken_ohlc" 5256000 # 2 months
 
 bq ls dev > /dev/null || bq --location=EU mk dev
-create "dev.oanda_prices" 1210000  # 2 weeks
+create "dev.kraken_ohlc" 1210000  # 2 weeks
--- bin/bigquery/remove_kraken_prices.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bq rm -f dev.kraken_prices
-bq rm -f test.kraken_prices
-bq rm -f prod.kraken_prices
