1f68eed0593134bff2314416c4138604797275e3
Olivier Picard <olivier_picard@icloud.com>
2021-05-22 15:48:50 +0400

    Clean code

--- services/health_mongo/main.py
+++ services/health_mongo/main.py
@@ -7,7 +7,7 @@ import traceback
 import firestore
 
 ENVIRONMENT = os.getenv('BRUSHED_CHARTS_ENVIRONMENT')
-REFRESH_RATE =  int(os.getenv('HEALTH_MONGO_REFRESH_RATE'))  # seconds
+REFRESH_RATE = int(os.getenv('HEALTH_MONGO_REFRESH_RATE'))  # seconds
 
 
 def build_status() -> list[dict]:
