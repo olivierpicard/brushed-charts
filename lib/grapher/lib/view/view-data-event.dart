b8d829aff681805475f72a2ee52750d8ede60949
Olivier Picard <olivier_picard@icloud.com>
2021-08-05 03:07:44 +0400

    Clean view module

--- lib/grapher/lib/view/view-data-event.dart
+++
@@ -1,8 +0,0 @@
-import 'package:grapher/filter/dataStruct/data2D.dart';
-import 'package:grapher/kernel/drawEvent.dart';
-
-class DrawDataMix {
-  final DrawEvent drawEvent;
-  final Iterable<Data2D> data;
-  DrawDataMix(this.drawEvent, this.data);
-}
