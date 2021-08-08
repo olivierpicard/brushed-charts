b70f67037966de07654652e5afd57a60e0ca31e4
Olivier Picard <olivier_picard@icloud.com>
2021-08-08 23:16:59 +0400

    Geometry: first try, make compatibility with DrawUnit Also add example

--- lib/grapher/lib/geometry/candlestick.dart
+++ lib/grapher/lib/geometry/candlestick.dart
@@ -1,11 +1,33 @@
-import 'package:grapher/filter/dataStruct/data2D.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:grapher/drawUnit/instanciable.dart';
+import 'package:grapher/drawUnit/unit-draw-event.dart';
+import 'package:grapher/filter/dataStruct/ohlc.dart';
 import 'package:grapher/geometry/geometry.dart';
 import 'package:grapher/kernel/propagator/endline.dart';
 
 class Candlestick extends Geometry with EndlinePropagator {
-  Candlestick({required Data2D data}) : super(data: data);
+  static const double BODY_PERCENT = 80;
+  static const double WICK_WIDTH = 3;
+  Candlestick() : super(BODY_PERCENT);
 
-  static Candlestick instanciate(Data2D data) {
-    return Candlestick(data: data);
+  @override
+  void draw(DrawUnitEvent event) {
+    final canvas = event.canvas;
+    final ohlc = event.unitData.y as OHLC;
+    final bodyWidth = event.width;
+    final body = Rect.fromLTRB(0, ohlc.close, bodyWidth, ohlc.open);
+    final wick = Rect.fromLTRB(bodyWidth / 2 - WICK_WIDTH / 2, ohlc.high,
+        bodyWidth / 2 - WICK_WIDTH / 2, ohlc.low);
+    canvas.drawRect(body, getColor(ohlc));
+    canvas.drawRect(wick, Paint()..color = Colors.grey);
   }
+
+  Paint getColor(OHLC ohlc) {
+    if (ohlc.open < ohlc.close) return Paint()..color = Colors.red;
+    return Paint()..color = Colors.green;
+  }
+
+  @override
+  DrawUnitObject instanciate() => Candlestick();
 }
--- lib/grapher/lib/geometry/geometry.dart
+++ lib/grapher/lib/geometry/geometry.dart
@@ -1,22 +1,13 @@
-import 'package:flutter/material.dart';
-import 'package:grapher/filter/dataStruct/data2D.dart';
-import 'package:grapher/kernel/drawable.dart';
+import 'package:grapher/drawUnit/instanciable.dart';
+import 'package:grapher/drawUnit/unit-draw-event.dart';
+import 'package:grapher/drawUnit/unit-drawable.dart';
 import 'package:grapher/kernel/propagator/endline.dart';
-import 'draw-unit-event.dart';
 
-typedef GeometryInstanciator = Geometry Function(Data2D);
+abstract class Geometry extends UnitDrawable
+    with EndlinePropagator, DrawUnitObject {
+  final double widthPercent;
 
-abstract class Geometry extends Drawable with EndlinePropagator {
-  final Data2D data;
+  Geometry(this.widthPercent) {}
 
-  Geometry({required this.data});
-
-  @override
-  void draw(covariant DrawUnitEvent drawEvent) {
-    final cursor = drawEvent.cursor;
-    drawEvent.canvas.drawRect(
-        Rect.fromCenter(
-            center: Offset(cursor, 0), width: drawEvent.length / 2, height: 50),
-        Paint()..color = Colors.red);
-  }
+  void draw(DrawUnitEvent event);
 }
