9250a862f50fe4630ca810733923f596d14197bd
Olivier Picard <olivier_picard@icloud.com>
2021-08-22 18:02:43 +0400

    Filter: Candle y is cast to OHLC and not dynamic

--- lib/grapher/lib/filter/dataStruct/candle.dart
+++ lib/grapher/lib/filter/dataStruct/candle.dart
@@ -3,6 +3,8 @@ import 'package:grapher/filter/dataStruct/timeseries2D.dart';
 
 class Candle2D extends Timeseries2D {
   double yMin, yMax;
+  OHLC get y => super.y as OHLC;
+
   Candle2D(DateTime x, OHLC y)
       : yMin = y.low,
         yMax = y.high,
