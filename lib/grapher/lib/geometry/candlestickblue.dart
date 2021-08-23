89653ad2f276a759cfa5b24a1254fe510e6e39d6
Olivier Picard <olivier_picard@icloud.com>
2021-08-23 22:24:29 +0400

    Geometry: add blue candle for test purpose only

---
+++ lib/grapher/lib/geometry/candlestickblue.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:grapher/drawUnit/instanciable.dart';
+import 'package:grapher/drawUnit/unit-draw-event.dart';
+import 'package:grapher/filter/dataStruct/ohlc.dart';
+import 'package:grapher/geometry/geometry.dart';
+import 'package:grapher/kernel/propagator/endline.dart';
+
+class CandlestickBlue extends Geometry with EndlinePropagator {
+  static const double BODY_PERCENT = 60;
+  static const double WICK_WIDTH = 2;
+
+  late final double bodyWidth;
+  late final OHLC ohlc;
+
+  CandlestickBlue() : super(BODY_PERCENT);
+
+  @override
+  void draw(DrawUnitEvent event) {
+    super.draw(event);
+    ohlc = event.unitData.y as OHLC;
+    bodyWidth = event.width;
+    drawWick(event);
+    drawBody(event);
+  }
+
+  void drawBody(DrawUnitEvent event) {
+    final bodyRect = Rect.fromLTRB(0, ohlc.close, bodyWidth, ohlc.open);
+    final paint = Paint()..color = getBodyColor(ohlc);
+    canvas!.drawRect(bodyRect, paint);
+  }
+
+  Color getBodyColor(OHLC ohlc) {
+    if (ohlc.open > ohlc.close) return Colors.lightBlue;
+    return Colors.white;
+  }
+
+  void drawWick(DrawUnitEvent event) {
+    final left = _wickLeftPosition;
+    final right = _wickRightPosition;
+    final bodyRect = Rect.fromLTRB(left, ohlc.high, right, ohlc.low);
+    final paint = Paint()..color = Colors.grey;
+    canvas!.drawRect(bodyRect, paint);
+  }
+
+  double get _wickLeftPosition => bodyWidth / 2 - WICK_WIDTH / 2;
+  double get _wickRightPosition => bodyWidth / 2 + WICK_WIDTH / 2;
+
+  @override
+  DrawUnitObject instanciate() => CandlestickBlue();
+}
