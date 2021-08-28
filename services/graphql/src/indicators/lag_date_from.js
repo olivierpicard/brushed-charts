7cafb370556be8052f481625494d96fad93fc5d0
Olivier Picard <olivier_picard@icloud.com>
2021-08-28 21:24:01 +0400

    graphql: prune data out of range using SQL

--- services/graphql/src/indicators/lag_date_from.js
+++ services/graphql/src/indicators/lag_date_from.js
@@ -2,15 +2,15 @@ const MILLISECONDS = 1000
 
 module.exports.update_dateFrom_with_lag = (args) => {
   const lag_date = get_lagged_date_from(args)
-  args['dateFrom'] = lag_date
+  args['dateFrom_lagged'] = lag_date
 
   return args
 }
 
 function get_lagged_date_from(args) {
-  update_dateFrom_to_utc(args)
+  const utc_dateFrom = dateFrom_to_utc(args)
   const delta_lag = window_size_to_seconds_lag(args)
-  const timestamp = get_lag_timestamp(args, delta_lag)
+  const timestamp = get_lag_timestamp(utc_dateFrom, delta_lag)
   const date_from = new Date(timestamp)
   const iso_lag_date = date_from.toISOString()
   
@@ -25,19 +25,18 @@ function window_size_to_seconds_lag(args) {
   return second_lag
 }
 
-function get_lag_timestamp(args, lag) {
-  const dateFrom = Date.parse(args['dateFrom'])
-  console.log(dateFrom)
-  const timestamp = dateFrom.valueOf()
-  console.log(timestamp)
+function get_lag_timestamp(utc_dateFrom, lag) {
+  const timestamp = Date.parse(utc_dateFrom)
   const lag_timestamp = timestamp - lag * MILLISECONDS
 
   return lag_timestamp
 }
 
-function update_dateFrom_to_utc(args) {
+function dateFrom_to_utc(args) {
   let dateFrom = args['dateFrom']
   dateFrom = dateFrom.toUpperCase()
   if (dateFrom.endsWith("Z")) return;
-  args['dateFrom'] += 'Z'
+  const utc_dateFrom = dateFrom + 'Z'
+
+  return utc_dateFrom
 }
--- services/graphql/src/indicators/moving_average.js
+++ services/graphql/src/indicators/moving_average.js
@@ -8,8 +8,8 @@ module.exports.moving_average = async (args) => {
   input = prepare_input(args);
   query = make_query(args)
   bq_rows = await fetch(query)
-  output = prepare_ouput(input['source'], bq_rows)
-  
+  output = prepare_ouput(input, bq_rows)
+
   return output
 }
 
@@ -24,43 +24,55 @@ function make_query(args) {
 
 function make_query_kraken(args) {
   return `
-  SELECT 
-      datetime,
-      UNIX_SECONDS(TIMESTAMP(datetime)) AS timestamp,
-      ROUND( 
-          AVG(${args['ohlcAttribute']}) OVER(
-          ORDER BY datetime
-          ROWS BETWEEN ${args['windowSize'] - 1} PRECEDING AND CURRENT ROW
-      ), 7) AS value
-  FROM prod.kraken_ohlc_interval(
-      ${args['granularity_origin']},
-      TIMESTAMP("${args['dateFrom']}"),
-      TIMESTAMP("${args['dateTo']}") )
-  WHERE 
-      asset_pair = "${args['asset']}"
-      AND granularity = ${args['granularity']}
-  order by datetime 
+  WITH moving_average AS(
+      SELECT
+          datetime,
+          UNIX_SECONDS(TIMESTAMP(datetime)) AS timestamp,
+          ROUND( 
+              AVG(${args['ohlcAttribute']}) OVER(
+              ORDER BY datetime
+              ROWS BETWEEN ${args['windowSize'] - 1} PRECEDING AND CURRENT ROW
+          ), 7) AS value
+      FROM prod.kraken_ohlc_interval(
+          ${args['granularity_origin']},
+          TIMESTAMP("${args['dateFrom_lagged']}"),
+          TIMESTAMP("${args['dateTo']}") )
+      WHERE 
+          asset_pair = "${args['asset']}"
+          AND granularity = ${args['granularity']}
+      ORDER BY datetime
+  )
+
+  SELECT *
+  FROM moving_average
+  WHERE datetime >= "${args['dateFrom']}"
   `
 }
 
 function make_query_oanda(args) {
   return `
-  SELECT 
-      date,
-      UNIX_SECONDS(TIMESTAMP(date)) AS timestamp,
-      ROUND( 
-          AVG(mid.${args['ohlcAttribute']}) OVER(
-          ORDER BY date
-          ROWS BETWEEN ${args['windowSize'] - 1} PRECEDING AND CURRENT ROW
-      ), 7) AS value
-  FROM prod.oanda_prices_interval(
-      ${args['granularity_origin']},
-      TIMESTAMP("${args['dateFrom']}"),
-      TIMESTAMP("${args['dateTo']}") )
-  WHERE 
-      instrument = "${args['asset']}"
-      AND granularity = "${args['granularity']}"
-  order by date
+  WITH moving_average AS(
+      SELECT 
+          date,
+          UNIX_SECONDS(TIMESTAMP(date)) AS timestamp,
+          ROUND( 
+              AVG(mid.${args['ohlcAttribute']}) OVER(
+              ORDER BY date
+              ROWS BETWEEN ${args['windowSize'] - 1} PRECEDING AND CURRENT ROW
+          ), 7) AS value
+      FROM prod.oanda_prices_interval(
+          ${args['granularity_origin']},
+          TIMESTAMP("${args['dateFrom_lagged']}"),
+          TIMESTAMP("${args['dateTo']}") )
+      WHERE 
+          instrument = "${args['asset']}"
+          AND granularity = "${args['granularity']}"
+      order by date
+  )
+
+  SELECT *
+  FROM moving_average
+  WHERE date >= "${args['dateFrom']}"
   `
 }
 
