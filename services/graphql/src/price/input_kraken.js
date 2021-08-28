f5302141f8b916d8a8560e1d6311ff7a91bfe54c
Olivier Picard <olivier_picard@icloud.com>
2021-08-28 19:48:09 +0400

    graphql: fix kraken granularity to static  1, 60, 1440

--- services/graphql/src/price/input_kraken.js
+++ services/graphql/src/price/input_kraken.js
@@ -6,9 +6,17 @@ module.exports.prepare_kraken_input = (args) => {
 
 
 function format_granularity(args) {
-  granularity = args['granularity']
-  granularity = Math.round(granularity / 60)
-  args['granularity'] = granularity
+  let formated_granularity = 0
+  const granularity = args['granularity']
+
+  if (granularity >= 60 && granularity < 3600) {
+    formated_granularity = 1
+  } else if (granularity >= 3600 && granularity < 86400) {
+    formated_granularity = 60
+  } else if (granularity >= 86400) {
+    formated_granularity = 1440
+  }
+  args['granularity'] = formated_granularity;
 
   return args
 }
