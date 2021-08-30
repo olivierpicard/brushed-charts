559d8104aa979cd951f4c1e1b25855577cbe9f8b
Olivier Picard <olivier_picard@icloud.com>
2021-08-30 22:54:20 +0400

    Remove old drafts

--- draft/old_labelling_helper/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use('/', express.static('./public'))
-
-app.listen(3331, () => {
-    console.log('Server app listening on port 3331');
-});
