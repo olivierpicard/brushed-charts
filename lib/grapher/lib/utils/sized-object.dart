de6c7c58c1da4006d31730b87515755af2f66051
Olivier Picard <olivier_picard@icloud.com>
2021-11-01 19:04:09 +0400

    Fix a bug in sizedObject that doesn't propagate drawEvent

--- lib/grapher/lib/utils/sized-object.dart
+++ lib/grapher/lib/utils/sized-object.dart
@@ -1,8 +1,15 @@
 import 'package:grapher/flex/object.dart';
+import 'package:grapher/kernel/drawEvent.dart';
 import 'package:grapher/kernel/object.dart';
 import 'package:grapher/kernel/propagator/single.dart';
 
 class SizedObject extends FlexObject with SinglePropagator {
   GraphObject? child;
   SizedObject({String length = 'auto', this.child}) : super(length: length);
+
+  @override
+  void draw(DrawEvent event) {
+    super.draw(event);
+    propagate(event);
+  }
 }
