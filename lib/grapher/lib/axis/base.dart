f32b4dabe88b169f1ba610683b6e3fb82df0c362
Olivier Picard <olivier_picard@icloud.com>
2021-11-01 19:04:44 +0400

    Abstract axisObject draw background

--- lib/grapher/lib/axis/base.dart
+++ lib/grapher/lib/axis/base.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:grapher/flex/object.dart';
+import 'package:grapher/kernel/drawEvent.dart';
 import 'package:grapher/kernel/drawable.dart';
 import 'package:grapher/kernel/object.dart';
 import 'package:grapher/kernel/propagator/single.dart';
@@ -6,4 +10,16 @@ import 'package:grapher/kernel/propagator/single.dart';
 abstract class AxisObject extends Drawable with SinglePropagator {
   final GraphObject? child;
   AxisObject(this.child);
+
+  @override
+  void draw(DrawEvent event) {
+    super.draw(event);
+    drawBackground(event);
+  }
+
+  void drawBackground(DrawEvent event) {
+    final zone = event.drawZone.toRect;
+    final paint = Paint()..color = Colors.black;
+    canvas!.drawRect(zone, paint);
+  }
 }
