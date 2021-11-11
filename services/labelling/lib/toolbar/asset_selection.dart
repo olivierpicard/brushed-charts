24038c0219209d3b22e24eec43eb89a4f5d78f18
olivierpicard <>
2021-11-11 17:27:50 +0400

    AssetSelector save it's current value

--- services/labelling/lib/toolbar/asset_selection.dart
+++ services/labelling/lib/toolbar/asset_selection.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:labelling/toolbar/download_info.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AssetSelector extends StatefulWidget {
   final DownloadInfo data;
@@ -15,6 +16,22 @@ class AssetSelector extends StatefulWidget {
 
 class _AssetSelectorState extends State<AssetSelector> {
   final _controller = TextEditingController(text: DownloadInfo.defaultAsset);
+
+  @override
+  void initState() {
+    super.initState();
+    _loadPref();
+  }
+
+  Future<void> _loadPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedAsset = prefs.getString('asset');
+    setState(() {
+      widget.data.asset = savedAsset ?? DownloadInfo.defaultAsset;
+      _controller.text = widget.data.asset;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -26,10 +43,13 @@ class _AssetSelectorState extends State<AssetSelector> {
   }
 
   void _onEdited(String? asset) {
-    if (asset == null) return;
-    setState(() {
-      widget.data.asset = asset;
-    });
+    setState(() => widget.data.asset = asset ?? DownloadInfo.defaultAsset);
+    _savePref();
     widget.onUpdate();
+  }
+
+  Future<void> _savePref() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('asset', widget.data.asset);
   }
 }
