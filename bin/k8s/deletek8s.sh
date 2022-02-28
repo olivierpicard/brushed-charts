4dba8b042bcac3cf76141d2c9fe85a6ce6040b35
olivierpicard <>
2022-02-28 21:41:01 +0400

    Don't wait for pod to be deleted

--- bin/k8s/deletek8s.sh
+++ bin/k8s/deletek8s.sh
@@ -14,5 +14,5 @@ rm -rf $BUILD_DIR
 mkdir $BUILD_DIR 2> /dev/null
 cp -r kubernetes/* $BUILD_DIR/
 grep -Rl '{{PROFIL}}' $BUILD_DIR | xargs sed -i "s/{{PROFIL}}/$PROFIL/g" 
-microk8s kubectl delete -f $BUILD_DIR --grace-period=2
+microk8s kubectl delete -f $BUILD_DIR --grace-period=2 --wait=false
 microk8s kubectl delete pvc --all
