193c6009780c144fa947975f9ae5001b91dec2af
olivierpicard <>
2022-03-06 13:23:13 +0400

    Use microk8s rather than kubectl directly

--- bin/k8s/config-gcr-on-k8s.sh
+++ bin/k8s/config-gcr-on-k8s.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl create secret docker-registry --dry-run=client -o yaml gcr-json-key \
+microk8s kubectl create secret docker-registry --dry-run=client -o yaml gcr-json-key \
     --docker-server=europe-docker.pkg.dev \
     --docker-username=_json_key \
     --docker-password="$(cat /etc/brushed-charts/gcp-accservice_artifact-reader.json)" \
