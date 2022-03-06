e5a4be9f75946df0a06821e97538af2a68eaadcd
olivierpicard <>
2022-03-06 11:23:53 +0400

    Create a new publish methode. With defined dev and prod

--- bin/k8s/config-gcr-on-k8s.sh
+++ bin/k8s/config-gcr-on-k8s.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-microk8s kubectl create secret docker-registry --dry-run=client -o yaml gcr-json-key \
+kubectl create secret docker-registry --dry-run=client -o yaml gcr-json-key \
     --docker-server=europe-docker.pkg.dev \
     --docker-username=_json_key \
     --docker-password="$(cat /etc/brushed-charts/gcp-accservice_artifact-reader.json)" \
     --docker-email=artifact-reader@brushed-charts.iam.gserviceaccount.com \
-    | microk8s kubectl apply -f -
+    | kubectl apply -f -
