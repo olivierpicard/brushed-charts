aa0bdb1db55047664e2fc94271df439cf9dc5c11
olivierpicard <>
2022-06-19 20:08:12 +0400

    Provision testing infrastructure

--- terraform/gke-autopilot-paris/main.tf
+++ terraform/gke-autopilot-paris/main.tf
@@ -14,10 +14,12 @@ terraform {
   }
 }
 
+
 provider "google" {
   project = "brushed-charts"
   region  = "europe-west9"
 }
+
 
 resource "google_container_cluster" "gke_autopilot_paris" {
   name     = "gke-autopilot-paris"
@@ -29,3 +31,27 @@ resource "google_container_cluster" "gke_autopilot_paris" {
     prevent_destroy = true
   }
 }
+
+
+output "gke-endpoint" {
+  value       = google_container_cluster.gke_autopilot_paris.endpoint
+  description = "URL to access to GKE autopilot Paris cluster"
+}
+
+output "gke-certificate" {
+  value       = google_container_cluster.gke_autopilot_paris.master_auth.0.client_certificate
+  description = "Public certificate of GKE autopilot Paris cluster"
+}
+
+output "gke-client_key" {
+  value       = google_container_cluster.gke_autopilot_paris.master_auth.0.client_key
+  description = "Client private key for GKE autopilot Paris cluster"
+  sensitive   = true
+}
+
+output "gke-cluster_ca_certificate" {
+  value       = google_container_cluster.gke_autopilot_paris.master_auth.0.cluster_ca_certificate
+  description = "Cluster public CA Certificate for GKE autopilot Paris cluster"
+}
+
+
